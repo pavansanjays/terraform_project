cb1f72495d71664736a9747dce952ff9dc5c9aa0
pavansanjays <pavanburada8@gmail.com>
2026-06-06 16:48:40 +0000

    changing dev,stag.yamls

--- terraform-files/aws-vpc/dynamodb.tf
+++ terraform-files/aws-vpc/dynamodb.tf
@@ -1,13 +1,13 @@
-resource "aws_dynamodb_table" "dynamodb_table" {
-  name = "terraform-locks"
+# resource "aws_dynamodb_table" "dynamodb_table" {
+#   name = "terraform-locks"
 
-  billing_mode   = "PROVISIONED"
-  read_capacity  = 20
-  write_capacity = 20
-  hash_key       = "LockID"
+#   billing_mode   = "PROVISIONED"
+#   read_capacity  = 20
+#   write_capacity = 20
+#   hash_key       = "LockID"
 
-  attribute {
-    name = "LockID"
-    type = "S"
-  }
-}
+#   attribute {
+#     name = "LockID"
+#     type = "S"
+#   }
+# }
